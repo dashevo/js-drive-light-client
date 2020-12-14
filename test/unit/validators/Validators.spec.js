6b1f9769d0d2e16422e55b6c0264cfe14d7b22ee
Cofresi <cofresi@dash.org>
2020-12-14 14:35:45 -0400

    rem platformBlockHeight & linter errors

--- test/unit/validators/Validators.spec.js
+++ test/unit/validators/Validators.spec.js
@@ -1,9 +1,7 @@
-const { SimplifiedMNListStore } = require('@dashevo/dashcore-lib');
 const Validators = require('../../../lib/validators/Validators');
 const validatorFixtures = require('../../../lib/test/fixtures/validators');
 
 describe('Validators', () => {
-
   let smlMock;
   let smlStoreMock;
   let validators;
@@ -13,18 +11,18 @@ describe('Validators', () => {
   let platformBlockHeight;
   let coreBlockHeight;
   const fixedValidatorSet = {
-      version: 1,
-      llmqType: 4,
-      quorumHash: '0000000001e48ae85c4554329f3432f384d1bda5031e503a8065331bb5475902',
-      signersCount: 50,
-      signers: 'ffffffffffff03',
-      validMembersCount: 50,
-      validMembers: 'ffffffffffff03',
-      quorumPublicKey: '0e448ce49a3b1a89dae293d9566862e6815438ffd2d03e54bec4167e8db7770b8da8bbbf0dbd256206c35bca4bd0d524',
-      quorumVvecHash: 'ce6a3f0367c8300161bd45a20a394fbf4c8332d0c6e10c0ddb1675fb49a13031',
-      quorumSig: '81b212eb1107c9a8685a8d20c80b110e8e43333382838b17bda474d81d0028dee64974d9faf8ba5f2260b286ec9e0cef047e2e0cf1a52f6359d6ddc2237818ecfcda84de389aeaa3da232cd396bc65c5d1c8c25d8df4364a60ee0587b03f3f5a',
-      membersSig: '0509f9bae9ab601a4436db306aaeb57a70ae14b1ec18e0a80f4c3434295a7045b1f50f5b8a315ee068f182d3f728097911472087ccfcfb5d2f6154f6c6860e6d2d2e06bb4b7eb8239c1d17b0462df2a19132feed479286a9971b29c57809a78e'
-    };
+    version: 1,
+    llmqType: 4,
+    quorumHash: '0000000001e48ae85c4554329f3432f384d1bda5031e503a8065331bb5475902',
+    signersCount: 50,
+    signers: 'ffffffffffff03',
+    validMembersCount: 50,
+    validMembers: 'ffffffffffff03',
+    quorumPublicKey: '0e448ce49a3b1a89dae293d9566862e6815438ffd2d03e54bec4167e8db7770b8da8bbbf0dbd256206c35bca4bd0d524',
+    quorumVvecHash: 'ce6a3f0367c8300161bd45a20a394fbf4c8332d0c6e10c0ddb1675fb49a13031',
+    quorumSig: '81b212eb1107c9a8685a8d20c80b110e8e43333382838b17bda474d81d0028dee64974d9faf8ba5f2260b286ec9e0cef047e2e0cf1a52f6359d6ddc2237818ecfcda84de389aeaa3da232cd396bc65c5d1c8c25d8df4364a60ee0587b03f3f5a',
+    membersSig: '0509f9bae9ab601a4436db306aaeb57a70ae14b1ec18e0a80f4c3434295a7045b1f50f5b8a315ee068f182d3f728097911472087ccfcfb5d2f6154f6c6860e6d2d2e06bb4b7eb8239c1d17b0462df2a19132feed479286a9971b29c57809a78e',
+  };
 
   beforeEach(function beforeEach() {
     quorumFixtures = validatorFixtures.getQuorumFixtures();
@@ -37,9 +35,9 @@ describe('Validators', () => {
       getSMLbyHeight: this.sinon.stub().returns(smlMock),
       getCurrentSML: this.sinon.stub().returns(smlMock),
     };
-    //const diffArray = new Array(17);
-    //smlStoreMock = SimplifiedMNListStore(diffArray);
-    rotationSignature = Buffer.from(rotationSignatureString, 'hex')
+    // const diffArray = new Array(17);
+    // smlStoreMock = SimplifiedMNListStore(diffArray);
+    rotationSignature = Buffer.from(rotationSignatureString, 'hex');
     platformBlockHeight = 111;
     coreBlockHeight = 2222;
     validators = new Validators(smlStoreMock);
@@ -50,28 +48,34 @@ describe('Validators', () => {
       expect(validators.smlStore).to.deep.equal(smlStoreMock);
     });
     it('should throw an error if smlStore is not passed', () => {
-      expect(function () {
+      expect(() => {
         validators = new Validators(undefined);
       }).to.throw('Validators class requires smlStore as a parameter');
     });
   });
   describe('#getValidatorSet', () => {
     it('should get the latest validator set', async () => {
-      const validatorSet = await validators.getValidatorSet(rotationSignature, platformBlockHeight);
+      const validatorSet = await validators.getValidatorSet(rotationSignature);
       expect(validatorSet).to.deep.equal(fixedValidatorSet);
     });
     it('should get the latest validator set with only verified quorums', async () => {
-      const validatorSet = await validators.getValidatorSet(rotationSignature, platformBlockHeight, true);
+      const validatorSet = await validators.getValidatorSet(
+        rotationSignature, true,
+      );
       expect(validatorSet).to.deep.equal(fixedValidatorSet);
     });
   });
   describe('#getValidatorSetForCoreHeight', () => {
     it('should get the validator set for a specific core height ', async () => {
-      const validatorSetForHeight = await validators.getValidatorSetForCoreHeight(rotationSignature, platformBlockHeight, coreBlockHeight);
+      const validatorSetForHeight = await validators.getValidatorSetForCoreHeight(
+        rotationSignature, platformBlockHeight, coreBlockHeight,
+      );
       expect(validatorSetForHeight).to.deep.equal(fixedValidatorSet);
     });
     it('should get the validator set for a specific core height with only verified quorums', async () => {
-      const validatorSetForHeight = await validators.getValidatorSetForCoreHeight(rotationSignature, platformBlockHeight, coreBlockHeight, true);
+      const validatorSetForHeight = await validators.getValidatorSetForCoreHeight(
+        rotationSignature, platformBlockHeight, coreBlockHeight, true,
+      );
       expect(validatorSetForHeight).to.deep.equal(fixedValidatorSet);
     });
   });
